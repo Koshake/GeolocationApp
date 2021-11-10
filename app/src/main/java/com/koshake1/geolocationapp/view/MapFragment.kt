473f01ae6aca8473da1c55772db6d81e08c60a0e
Koshake <koshake1@mail.com>
2021-11-10 09:54:37 +0300

    feature: map fragment initialisation feature: map initialisation feature: adding markers

--- app/src/main/java/com/koshake1/geolocationapp/view/MapFragment.kt
+++ app/src/main/java/com/koshake1/geolocationapp/view/MapFragment.kt
@@ -1,4 +1,55 @@
 package com.koshake1.geolocationapp.view
 
-class MapFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import com.koshake1.geolocationapp.R
+
+class MapFragment : Fragment(), OnMapReadyCallback {
+
+    companion object {
+        fun newInstance() = MapFragment()
+
+        const val MAP_ZOOM = 5f
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        return inflater.inflate(R.layout.fragment_map, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initMap()
+    }
+
+    private fun initMap() {
+        val mapFragment = childFragmentManager.findFragmentById(R.id.map) as? SupportMapFragment
+        mapFragment?.getMapAsync(this)
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        googleMap.setOnMapClickListener { latlng ->
+            googleMap.clear()
+
+            googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latlng, MAP_ZOOM))
+
+            val location = LatLng(latlng.latitude, latlng.longitude)
+            googleMap.addMarker(MarkerOptions()
+                .position(location)
+                .title("${latlng.latitude} : ${latlng.longitude}"))
+        }
+    }
 }
